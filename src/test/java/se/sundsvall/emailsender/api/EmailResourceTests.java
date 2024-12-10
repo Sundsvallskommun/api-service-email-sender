57847cb713b15b7df4c684c939e664f356b6bcad
cheezi0747 <linus.sjolinder@b3.se>
2024-12-10 10:55:44 +0100

    UF-11274: Fix deperacted mockbean

--- src/test/java/se/sundsvall/emailsender/api/EmailResourceTests.java
+++ src/test/java/se/sundsvall/emailsender/api/EmailResourceTests.java
@@ -10,7 +10,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import se.sundsvall.emailsender.Application;
@@ -28,7 +28,7 @@ class EmailResourceTests {
 	@Autowired
 	private WebTestClient webTestClient;
 
-	@MockBean
+	@MockitoBean
 	private EmailService serviceMock;
 
 	@Captor
